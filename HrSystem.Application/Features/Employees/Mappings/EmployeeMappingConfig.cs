a3dafa18c9a025ff1ed972f34c96b73e203ec6b6
Youssef Ghonem <youssef.fcaih@gmail.com>
2026-01-30 11:58:57 +0200

    fixes

--- HrSystem.Application/Features/Employees/Mappings/EmployeeMappingConfig.cs
+++ HrSystem.Application/Features/Employees/Mappings/EmployeeMappingConfig.cs
@@ -17,7 +17,15 @@ public class EmployeeMappingConfig : IRegister
             .Map(dest => dest.DirectManagerName, src => src.DirectManager != null ? src.DirectManager.FullNameEn : null)
             .Map(dest => dest.BranchName, src => src.Branch != null ? src.Branch.NameEn : null)
             .Map(dest => dest.FullNameAr, src => src.FullNameAr)
-            .Map(dest => dest.FullNameEn, src => src.FullNameEn);
+            .Map(dest => dest.FullNameEn, src => src.FullNameEn)
+            .Map(dest => dest.GenderNameEn, src => src.Gender != null ? src.Gender.NameEn : null)
+            .Map(dest => dest.GenderNameAr, src => src.Gender != null ? src.Gender.NameAr : null)
+            .Map(dest => dest.MaritalStatusNameEn, src => src.MaritalStatus != null ? src.MaritalStatus.NameEn : null)
+            .Map(dest => dest.MaritalStatusNameAr, src => src.MaritalStatus != null ? src.MaritalStatus.NameAr : null)
+            .Map(dest => dest.ContractTypeNameEn, src => src.ContractType != null ? src.ContractType.NameEn : null)
+            .Map(dest => dest.ContractTypeNameAr, src => src.ContractType != null ? src.ContractType.NameAr : null)
+            .Map(dest => dest.StatusNameEn, src => src.Status != null ? src.Status.NameEn : null)
+            .Map(dest => dest.StatusNameAr, src => src.Status != null ? src.Status.NameAr : null);
 
         config.NewConfig<Employee, EmployeeListDto>()
             .Map(dest => dest.FullNameEn, src => src.FullNameEn)
