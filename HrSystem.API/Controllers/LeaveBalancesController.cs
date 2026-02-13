8b9b28df3593599ca5b4892d6d0d74d36502ba0a
Youssef Ghonem <youssef.fcaih@gmail.com>
2026-02-13 16:22:50 +0200

    fix

--- HrSystem.API/Controllers/LeaveBalancesController.cs
+++ HrSystem.API/Controllers/LeaveBalancesController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HrSystem.API.Controllers;
 
-[Authorize(Roles = $"{RoleNames.OrganizationAdmin},{RoleNames.HRManager},{RoleNames.HRSpecialist}")]
+[Authorize]
 [Route("api/[controller]")]
 public class LeaveBalancesController : APIBaseController
 {
